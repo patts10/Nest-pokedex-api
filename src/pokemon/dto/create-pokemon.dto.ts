68c04ec3acc90a90941b0c70c26cfd9ba3a464af
Christian Mendoza <patts2810@gmail.com>
2022-11-12 11:41:56 -0500

    fix: delete linter packages

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,12 @@
-export class CreatePokemonDto {}
+import { IsInt, IsPositive, IsString, Min, MinLength } from 'class-validator';
+
+export class CreatePokemonDto {
+  @IsPositive()
+  @IsInt()
+  @Min(1)
+  pokemonNumber: number;
+
+  @IsString()
+  @MinLength(1)
+  name: string;
+}
